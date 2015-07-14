dd8ab460125ff1f23f2cade6bd58ecdf7db94b3f
Ryan Frith <ryan.frith@icloud.com>
2015-07-14 04:24:40 -0400

    Added scaffolding and search form

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,11 +1,19 @@
 class ShortUrl < ActiveRecord::Base
 
-  validates_presence_of  :short, :long
-  validates_uniqueness_of :short, :long
-  validates_format_of :long, :short, :with => URI::regexp(%w(http https))
+  validates_presence_of :short, :if => :long?, :message => "An error occurred creating the shortened URL."
+  validates_presence_of :long, :message => "You must supply a value for the original URL"
+  validates_uniqueness_of :short, :long, :message => "Must be unique"
+  format_err = "Please enter a properly formatted URL"
+  validates_format_of :short, :with => URI::regexp(%w(http https)), :if => :long?, :message => format_err
+  validates_format_of :long, :with => URI::regexp(%w(http https)), :message => format_err
 
   before_validation do
     shorten if !self.long.blank? and self.short.blank?
+  end
+
+
+  def self.search(query)
+    where("long like ? or short like ?", "%#{query}%", "%#{query}%")
   end
 
 
@@ -18,14 +26,17 @@ class ShortUrl < ActiveRecord::Base
   def generate_unique_short_url(long)
     short = UrlShortener::generate_short_url(long)
 
-    #replace zero with 'O' and upcase 'o' to '0' to  fulfill requirement (see Readme.rdoc)
-    short.gsub!(/[0o]/, 'O')
+    #replace zero with 'O' and upcase 'o' to 'O' to  fulfill requirement (see Readme.rdoc)
+    key = short.chars.last(UrlShortener::UNIQUE_KEY_LENGTH).join
+    short.chomp!(key)
+    key.gsub!(/[0o]/, 'O')
+    short += key
 
     if(ShortUrl.where(short: short ).count > 0) #try again if already in database
       generate_unique_short_url(long)
     else
       ShortUrl.all.each do |url|
-        if one_different_char?(short, url) #try again if the generated value is similar to existing value in database (see Readme.rdoc)
+        if one_different_char?(short, url.short) #try again if the generated value is similar to existing value in database (see Readme.rdoc)
           generate_unique_short_url(long)
         end
       end
---
+++ app/views/short_urls/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @short_url, :id, :created_at, :updated_at
