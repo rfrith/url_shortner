c6ce1009dacf7d1c16a806635d708f295dbfd2d2
Ryan Frith <ryan.frith@icloud.com>
2015-07-15 14:18:04 -0400

    Changed UrlShortener module to get config from secrets.yml

--- lib/url_shortener.rb
+++ lib/url_shortener.rb
@@ -1,9 +1,8 @@
 module UrlShortener
-  DOMAIN = "#{Rails::Server.new.options[:Host]}:#{Rails::Server.new.options[:Port]}"
-  HTTP = "http://"
-  UNIQUE_KEY_LENGTH = 5
+  DOMAIN = Rails.application.secrets.domain
+  UNIQUE_KEY_LENGTH = Rails.application.secrets.unique_key_length
   KEY_CHARS = ('a'..'z').to_a + (0..9).to_a
-  BLACK_LIST = ["foo","bar"]
+  BLACK_LIST = Rails.application.secrets.black_list.split(',')
 
   def generate_short_url(long_url)
     key_is_valid = false
@@ -14,7 +13,7 @@ module UrlShortener
         break if(!key_is_valid)
       end
     end
-    HTTP + DOMAIN + "/#{key}"
+    DOMAIN + "/#{key}"
   end
 
   module_function :generate_short_url
